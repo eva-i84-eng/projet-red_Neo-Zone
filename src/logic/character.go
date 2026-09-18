0eabb68b4cb33182ed3ed4615031f727def3dd62
Amine <amine_30220@icloud.com>
2026-09-18 07:38:08 +0000

    merge main into character.go, remove separate main.go

--- src/logic/character.go
+++ src/logic/character.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Character struct {
 	Name      string
 	Classe    string
@@ -7,4 +9,21 @@ type Character struct {
 	MaxHP     int
 	CurrentHP int
 	Inventory []string
+}
+
+func main() {
+	noah := Character{
+		Name:      "Noah",
+		Classe:    "vagabond",
+		Lvl:       1,
+		MaxHP:     100,
+		CurrentHP: 80,
+		Inventory: []string{"Katana Laser", "Revolver XRAY", "Fusil nucleaire"},
+	}
+
+	fmt.Println("Nom :", noah.Name)
+	fmt.Println("Classe :", noah.Classe)
+	fmt.Println("Niveau :", noah.Lvl)
+	fmt.Println("PV :", noah.CurrentHP, "/", noah.MaxHP)
+	fmt.Println("Inventaire :", noah.Inventory)
 }
