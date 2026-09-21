e627a93956ddfcf4a6d9eeb011c349fae57a0504
Amine <amine_30220@icloud.com>
2026-09-21 08:23:10 +0000

    add addItem, skill and spellBook functions; ignore .DS_Store

--- src/logic/character.go
+++ src/logic/character.go
@@ -109,3 +109,31 @@ func AccessInventory(c Character) {
 		}
 	}
 }
+
+func addItem(c *Character, item string) {
+	if len(c.Inventory) >= 10 {
+		fmt.Println("L'inventaire est plein. Nous ne pouvons accepter :", item)
+		return
+	}
+	c.Inventory = append(c.Inventory, item)
+}
+
+func skill(c *Character) {
+	if c.Classe == "Samurai" {
+		c.Sorts = []string{"Tempete du ninja", "Coup de poing"}
+		return
+	} else if c.Classe == "Cowboy" {
+		c.Sorts = []string{"Slowing Time", "Coup de poing"}
+		return
+	}
+	c.Sorts = []string{"Coup de poing"}
+}
+
+func spellBook(c *Character) {
+	for _, j := range c.Sorts {
+		if j == "Boule de Feu" {
+			return
+		}
+	}
+	c.Sorts = append(c.Sorts, "Boule de Feu")
+}
