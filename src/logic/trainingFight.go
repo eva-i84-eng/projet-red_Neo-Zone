d266488c1b9aba0d494fb2276489599a6a773ab8
8r1c3-333 <ton.email@example.com>
2026-09-22 12:10:16 +0200

    tâche 22 complète

--- src/logic/trainingFight.go
+++ src/logic/trainingFight.go
@@ -3,15 +3,21 @@ package main
 
 import "fmt"
 
-func trainingFight(player *Character){
+func trainingFight(player *Character) {
 	var goblin Monster
-	initgoblin(&goblin)
+	initGoblin(&goblin)
 	turn := 1
 	for player.CurrentHP > 0 && goblin.CurrentHP > 0 {
-		characterTurn(player, goblin)
+		fmt.Println("Tour", turn)
+		characterTurn(player, &goblin)
 		if goblin.CurrentHP > 0 {
-			goblinPattern(goblin, player, turn)
+			goblinPattern(&goblin, player, turn)
 		}
 		turn++
+	}
+	if player.CurrentHP <= 0 {
+		fmt.Println("Même en entraînement, tu te rates.")
+	} else {
+		fmt.Println("Victoire. C'est rarissime !")
 	}
 }
